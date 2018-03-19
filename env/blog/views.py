23f6c946044f298d8c678c9ced6e6fa034690c10
ZD <384925349@qq.com>
2018-03-19 09:58:02 +0800

    3.19更新

--- env/blog/views.py
+++ env/blog/views.py
@@ -6,17 +6,22 @@ def blog_list(request):
     context = {}
     context['blogs'] = Blog.objects.all()
     context['blog_types'] = BlogType.objects.all()
+
     return render_to_response('blog/blog_list.html', context)
 
 
-def blog_detail(request,blog_pk):
+def blog_detail(request, blog_pk):
     context = {}
+    context['blogs'] = Blog.objects.all()
+    context['blog_types'] = BlogType.objects.all()
     context['blog'] = get_object_or_404(Blog, pk=blog_pk)
+    context['blog_titles'] = Blog.objects.all()
     return render_to_response('blog/blog_detail.html', context)
 
 def blogs_with_type(request, blog_type_pk):
     context = {}
     blog_type = get_object_or_404(BlogType, pk=blog_type_pk)
-    context['blogs'] = Blog.objects.filter(blog_type=blog_type)
     context['blog_type'] = blog_type
+    context['blogs'] = Blog.objects.filter(blog_type=blog_type)
+    context['blog_types'] = BlogType.objects.all()
     return render_to_response('blog/blogs_with_type.html', context)
