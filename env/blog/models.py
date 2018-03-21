b7a0f9200c6ae1d20cf015fb72bb97a054c71114
ZD <384925349@qq.com>
2018-03-21 20:28:55 +0800

    3.21更新

--- env/blog/models.py
+++ env/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+# from ckeditor.fields import RichTextField
 
 
 class BlogType(models.Model):
@@ -19,7 +20,8 @@ class Blog(models.Model):
 
     def __str__(self):
         return "<Blog: %s>" % self.title
-
+    class Meta:
+        ordering = ['-created_time']
 
 class Topic(models.Model):
     text = models.CharField(max_length=200)
--- env/blog/views.py
+++ env/blog/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render_to_response, get_object_or_404
+from django.core.paginator import Paginator
 from .models import Blog, BlogType
 
 
 def blog_list(request):
+    blogs_all_list = Blog.objects.all()
+    paginator = Paginator(blogs_all_list, 10)
+    page_num = request.GET.get('page', 1)  # 获取url页码
+    page_of_blogs = paginator.get_page(page_num)
+
     context = {}
-    context['blogs'] = Blog.objects.all()
+    # context['blogs'] = page_of_blogs.object_list
+    context['page_of_blogs'] = page_of_blogs
     context['blog_types'] = BlogType.objects.all()
 
     return render_to_response('blog/blog_list.html', context)
@@ -16,6 +23,7 @@ def blog_detail(request, blog_pk):
     context['blog_types'] = BlogType.objects.all()
     context['blog'] = get_object_or_404(Blog, pk=blog_pk)
     context['blog_titles'] = Blog.objects.all()
+
     return render_to_response('blog/blog_detail.html', context)
 
 def blogs_with_type(request, blog_type_pk):
